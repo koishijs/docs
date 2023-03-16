b0da85c62f18701aa62d33f1588803a1c4abe747
Shigma <shigma10826@gmail.com>
2023-03-17 02:32:40 +0800

    admin auth login

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -74,7 +74,7 @@ export default async () => defineConfig({
         items: [
           makeLink('指令系统', '/manual/usage/command.md'),
           makeLink('跨平台', '/manual/usage/platform.md'),
-          makeLink('用户管理', '/manual/usage/user.md'),
+          makeLink('权限管理', '/manual/usage/permission.md'),
           // makeLink('国际化', '/manual/usage/i18n.md'),
           // makeLink('过滤器', '/manual/usage/filter.md'),
           // makeLink('服务与依赖', '/manual/usage/service.md'),
