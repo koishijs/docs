3a42dedbb7da4a557acaa7366eaf88416a40f07b
Shigma <shigma10826@gmail.com>
2023-05-03 01:50:11 +0800

    i18n manifest

--- .vitepress/config/index.ts
+++ .vitepress/config/index.ts
@@ -6,12 +6,6 @@ export default async () => defineConfig({
   title: 'Koishi',
   description: '创建跨平台、可扩展、高性能的机器人',
 
-  head: [
-    ['link', { rel: 'icon', href: '/logo.png' }],
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['meta', { name: 'theme-color', content: '#5546a3' }],
-  ],
-
   locales: {
     'en-US': require('./en-US'),
     'zh-CN': require('./zh-CN'),
